518ad93b47033aaa35ff2506b71a84e3c7748bc1
Tsa5522 <huan1606@purdue.edu>
2023-06-16 15:07:31 +0800

    login bug fix

--- src/main/java/com/management/CompanyManagementSystem/Controller/loginController.java
+++ src/main/java/com/management/CompanyManagementSystem/Controller/loginController.java
@@ -6,24 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class loginController {
     private final EmployeeService employeeService;
-    private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public loginController(EmployeeService employeeService, AuthenticationManager authenticationManager) {
+    public loginController(EmployeeService employeeService) {
         this.employeeService = employeeService;
-        this.authenticationManager = authenticationManager;
     }
     @GetMapping({"/", "/login"})
     public String getLogin() {
